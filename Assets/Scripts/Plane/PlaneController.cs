c09214d5069ec556e6adcf6ec52571081e3f9563
Oktarian <oktotb@gmail.com>
2021-02-12 19:29:13 +0800

    Added plane

---
+++ Assets/Scripts/Plane/PlaneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlaneController : MonoBehaviour
+{
+
+    public float speed = 15f;
+    float vertical, horizontal;
+
+    [SerializeField]
+    TextMeshProUGUI displayText = null;
+
+    private void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+        float xMovement = - transform.rotation.z * Time.deltaTime * speed * 3f;
+        transform.position += new Vector3(xMovement, 0, 0);
+
+        speed -= transform.forward.y * Time.deltaTime * 13.0f;
+        
+        vertical = Mathf.Lerp(vertical, Input.GetAxis("Vertical"), Time.deltaTime * 2f);
+        horizontal = Mathf.Lerp(horizontal, Input.GetAxis("Horizontal"), Time.deltaTime * 2f);
+
+        transform.Rotate(vertical, horizontal, -horizontal);
+
+        displayText.text = "Altitude: " + (int)transform.position.y;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Flight Simulator
+
+Welcome to my flight simulator! 
+
+This is a project I built using C# and Unity, where you control a plane through an infinite procedurally generated terrain. To generate terrain, a combination of Perlin and Simplex noise is used to achieve a mountainous look with occasional water areas.
+
+
+![Images/Screenshot_1.png](Flight%20Simulator%2075f87fc6c02340b4b678d4e86d471851/Screenshot_1.png)
+
+
+![Images/Screenshot_2.png](Flight%20Simulator%2075f87fc6c02340b4b678d4e86d471851/Screenshot_2.png)
+
+
+
+Credit:
+
+- Sebastian Lague: foundation of terrain generation
+- Gasgiant: plane model
